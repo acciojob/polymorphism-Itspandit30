502a31e88e0edda2f331bf6869bfd54a019df97c
unknown <ashishcool2325@gmail.com>
2023-08-20 17:56:21 +0530

    CodeChanges

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
-package com.driver;
-
 public class Main {
 
+    public static void main(String[] args) {
+        Product p = new Product();
+
+        int result1 = p.product(2, 3);
+        System.out.println("Product of two integers: " + result1);
+
+        int result2 = p.product(2, 3, 4);
+        System.out.println("Product of three integers: " + result2);
+
+        double result3 = p.product(2.5, 3.5);
+        System.out.println("Product of two doubles: " + result3);
+    }
+
+    static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
 }
